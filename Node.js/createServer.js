532f4520e73cb0ba00dcd7273d8bddab914b073f
kyle-luo <tsjlky@gmail.com>
2019-09-02 01:14:45 -0400

    node

---
+++ Node.js/createServer.js
@@ -0,0 +1,30 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 4000;
+
+// const server = http.createServer((rep, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type', 'text/plain');
+// 	res.end('Hello World/n')
+// })
+
+// server.listen(port, hostname, () => {
+// 	console.log('Server running at http://${hostname}:${port}/')
+// })
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
