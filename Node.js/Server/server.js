b9a9ef3f82cfe6f5463eaea9ed1319eec8b2015e
kyle-luo <tsjlky@gmail.com>
2019-09-02 01:19:00 -0400

    Update server.js

--- Node.js/Server/server.js
+++ Node.js/Server/server.js
@@ -4,7 +4,19 @@ const config = require('./config').config;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello, World');
+	switch (req.url) {
+		case '/':
+			res.end('Hello, world');
+			break;
+		case '/about':
+			res.end('About page');
+			break;
+		case '/home':
+			res.end('Home page');
+			break;
+		default:
+			res.end('Not Found!');
+	}
 })
 
 server.listen(config.port, config.hostname,() => {
